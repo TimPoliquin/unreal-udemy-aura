047e572bdd0592dab7871d96c47faf165bc42119
Tim Poliquin <tim.poliquin@gmail.com>
2025-02-03 15:26:59 -0500

    2.7 - Character Blueprint Setup
    - Add weapon socket to base character
    - Add blueprints for Aura Character and Goblin with Spear

--- Source/Aura/Private/Character/AuraBaseCharacter.cpp
+++ Source/Aura/Private/Character/AuraBaseCharacter.cpp
@@ -6,6 +6,9 @@
 AAuraBaseCharacter::AAuraBaseCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraBaseCharacter::BeginPlay()
