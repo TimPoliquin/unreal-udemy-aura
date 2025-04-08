e6b8a19469a9b132a4dff2333173e3327f7aac02
Tim Poliquin <tim.poliquin@gmail.com>
2025-04-08 12:21:15 -0400

    Add POI indicator next to fishing pool
    https://github.com/TimPoliquin/unreal-udemy-aura/issues/9

---
+++ Source/Aura/Private/Actor/AuraPOI.cpp
@@ -0,0 +1,66 @@
+﻿// Copyright Alien Shores
+
+
+#include "Actor/AuraPOI.h"
+
+#include "Aura/Aura.h"
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+
+
+AAuraPOI::AAuraPOI()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Component"));
+	SphereComponent->SetCollisionObjectType(ECC_WorldStatic);
+	SphereComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComponent->SetGenerateOverlapEvents(true);
+	SphereComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	SetRootComponent(SphereComponent);
+	POIWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("POIWidget"));
+	POIWidget->SetupAttachment(GetRootComponent());
+	InteractionWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("InteractionWidget"));
+	InteractionWidget->SetupAttachment(GetRootComponent());
+}
+
+void AAuraPOI::BeginPlay()
+{
+	Super::BeginPlay();
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AAuraPOI::OnSphereBeginOverlap);
+	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &AAuraPOI::OnSphereEndOverlap);
+}
+
+void AAuraPOI::OnSphereBeginOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool BFromSweep,
+	const FHitResult& SweepResult
+)
+{
+	// if player, show interaction widget
+	if (IsPlayerActor(OtherActor))
+	{
+		ShowInteractionWidget();
+	}
+}
+
+void AAuraPOI::OnSphereEndOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex
+)
+{
+	// if player, hide interaction widget
+	if (IsPlayerActor(OtherActor))
+	{
+		HideInteractionWidget();
+	}
+}
+
+bool AAuraPOI::IsPlayerActor(const AActor* Actor) const
+{
+	return IsValid(Actor) && Actor->ActorHasTag(TAG_PLAYER);
+}
---
+++ Source/Aura/Public/Actor/AuraPOI.h
@@ -0,0 +1,54 @@
+﻿// Copyright Alien Shores
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraPOI.generated.h"
+
+class USphereComponent;
+class UWidgetComponent;
+
+UCLASS()
+class AURA_API AAuraPOI : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPOI();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="POI")
+	TObjectPtr<UWidgetComponent> POIWidget;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="POI")
+	TObjectPtr<UWidgetComponent> InteractionWidget;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="POI")
+	TObjectPtr<USphereComponent> SphereComponent;
+
+	UFUNCTION()
+	void OnSphereBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool BFromSweep,
+		const FHitResult& SweepResult
+	);
+	UFUNCTION()
+	void OnSphereEndOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex
+	);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void ShowInteractionWidget();
+	UFUNCTION(BlueprintImplementableEvent)
+	void HideInteractionWidget();
+
+private:
+	bool IsPlayerActor(const AActor* Actor) const;
+};
