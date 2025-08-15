11511e518d7312bb8abac325277566a31411bb94
Tim Poliquin <tim.poliquin@gmail.com>
2025-08-15 13:27:58 -0400

    Finish basic treasure chest implementation

---
+++ Source/Aura/Private/Material/AuraMaterialBlueprintLibrary.cpp
@@ -0,0 +1,51 @@
+﻿// Copyright Alien Shores
+
+
+#include "Material/AuraMaterialBlueprintLibrary.h"
+
+#include "Kismet/KismetMaterialLibrary.h"
+
+void UAuraMaterialBlueprintLibrary::ReplaceMaterialsWithDynamicMaterials(UMeshComponent* Mesh, TArray<UMaterialInterface*>& OriginalMaterials, TArray<UMaterialInstanceDynamic*>& DynamicMaterials)
+{
+	OriginalMaterials.Append(Mesh->GetMaterials());
+	for (int Idx = 0; Idx < Mesh->GetNumMaterials(); Idx++)
+	{
+		UMaterialInterface* Material = Mesh->GetMaterial(Idx);
+		if (UMaterialInstanceDynamic* DynamicMaterialInstance = Cast<UMaterialInstanceDynamic>(Material))
+		{
+			DynamicMaterials.Add(DynamicMaterialInstance);
+		}
+		else
+		{
+			DynamicMaterials.Add(
+				UKismetMaterialLibrary::CreateDynamicMaterialInstance(
+					Mesh,
+					Material
+				)
+			);
+		}
+		Mesh->SetMaterial(Idx, DynamicMaterials[Idx]);
+	}
+}
+
+void UAuraMaterialBlueprintLibrary::SetScalarParameterOnDynamicMaterials(UMeshComponent* Mesh, const FName Parameter, const float Value)
+{
+	for (UMaterialInterface* Material : Mesh->GetMaterials())
+	{
+		if (UMaterialInstanceDynamic* DynamicMaterialInstance = Cast<UMaterialInstanceDynamic>(Material))
+		{
+			DynamicMaterialInstance->SetScalarParameterValue(Parameter, Value);
+		}
+	}
+}
+
+void UAuraMaterialBlueprintLibrary::SetBooleanParameterOnDynamicMaterials(UMeshComponent* Mesh, const FName Parameter, const bool Value)
+{
+	for (UMaterialInterface* Material : Mesh->GetMaterials())
+	{
+		if (UMaterialInstanceDynamic* DynamicMaterialInstance = Cast<UMaterialInstanceDynamic>(Material))
+		{
+			DynamicMaterialInstance->SetScalarParameterValue(Parameter, Value);
+		}
+	}
+}
